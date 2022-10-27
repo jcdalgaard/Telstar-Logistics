35d784dfc7a1e179b59c413b5d7598432812ec64
Nguyen Tran Minh Khoa <ntmk@netcompany.com>
2022-10-27 15:30:15 +0200

    Add popular cities reporting chart

--- Frontend/telstar-webpage/src/views/BookingReport.vue
+++ Frontend/telstar-webpage/src/views/BookingReport.vue
@@ -1,5 +1,36 @@
 <template>
     <div class="view-container">
-        <h1>This is an Reporting page</h1>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <CardContainer :title="lang.reporting.popularCities">
+                    <PieChart :data-sets="chartData" />
+                </CardContainer>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import CardContainer from '@/components/CardContainer'
+import PieChart from '@/components/PieChart'
+import lang from '@/utils/lang/langBroker'
+
+export default {
+    components: { PieChart, CardContainer },
+    data() {
+        return {
+            chartData: [],
+            lang: lang,
+        }
+    },
+    mounted() {
+        // mockup
+        this.chartData = [
+            { label: 'Darfur', value: 74 },
+            { label: 'Congo', value: 37 },
+            { label: 'Slavekysten', value: 15 },
+            { label: 'Luanda', value: 46 },
+            { label: 'Other', value: 27 },
+        ]
+    },
+}
+</script>
