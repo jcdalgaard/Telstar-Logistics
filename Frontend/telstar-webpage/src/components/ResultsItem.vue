5e92c5b4eb55d28252c36835421fc0367e47be9a
Marius <marius.havnaas@drift.oslo.kommune.no>
2022-10-27 18:02:54 +0200

    Added functionality for listing routes.

---
+++ Frontend/telstar-webpage/src/components/ResultsItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <button
+        class="btn container-fluid bg-telstar-primary-light shadow-sm rounded my-2 p-3 text-start"
+        @click="$emit('click')"
+    >
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col-12 col-sm">
+                        <label>{{ lang.estimateLabel }}</label>
+                        <div class="row">
+                            <div class="col text-telstar-primary fw-bold">
+                                {{ formatDate(route.estimatedArrival) }}
+                            </div>
+                        </div>
+                    </div>
+                    <label class="col-12">{{ route.stops }} stops</label>
+                </div>
+            </div>
+            <div class="col-auto d-flex align-items-center">
+                <span class="text-telstar-primary fw-bold"
+                    >${{ route.price }}</span
+                >
+                <font-awesome-icon
+                    icon="fa-solid fa-chevron-right"
+                    class="ms-2"
+                />
+            </div>
+        </div>
+    </button>
+</template>
+
+<script>
+import lang from '@/utils/lang/langBroker'
+import dateTime from 'date-and-time'
+
+export default {
+    name: 'ResultsItem',
+    props: {
+        route: { type: Object, requred: true },
+    },
+    emits: ['click'],
+    data() {
+        return {
+            lang: lang.ResultsList.ResultsItem,
+        }
+    },
+    methods: {
+        formatDate(date) {
+            const thisDate = new Date(date)
+            if (dateTime.isValid(thisDate)) {
+                return dateTime.format(thisDate, 'HH:mm dddd, DD MMMM')
+            } else console.log(`${date} is not a vaild date string`)
+        },
+    },
+}
+</script>
+
+<style scoped></style>
