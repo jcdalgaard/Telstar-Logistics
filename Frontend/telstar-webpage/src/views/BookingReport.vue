3a907515f386b5854f59798965c95754821fce32
Nguyen Tran Minh Khoa <ntmk@netcompany.com>
2022-10-27 17:41:29 +0200

    Add popular routes chart

--- Frontend/telstar-webpage/src/views/BookingReport.vue
+++ Frontend/telstar-webpage/src/views/BookingReport.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="view-container">
-        <div class="row">
+        <div class="row gx-5 gy-4">
             <div class="col-12 col-lg-6">
                 <CardContainer :title="lang.reporting.popularCities">
-                    <PieChart :data-sets="chartData" />
+                    <PieChart :data-sets="popularCitiesChartData" />
+                </CardContainer>
+            </div>
+            <div class="col-12 col-lg-6">
+                <CardContainer :title="lang.reporting.popularRoutes">
+                    <BarChart
+                        :data-sets="popularRoutesChartData"
+                        :this-month-data-sets="popularRoutesThisMonthChartData"
+                    />
                 </CardContainer>
             </div>
         </div>
@@ -13,24 +21,36 @@
 import CardContainer from '@/components/CardContainer'
 import PieChart from '@/components/PieChart'
 import lang from '@/utils/lang/langBroker'
+import BarChart from '@/components/BarChart'
 
 export default {
-    components: { PieChart, CardContainer },
+    components: { BarChart, PieChart, CardContainer },
     data() {
         return {
-            chartData: [],
+            popularCitiesChartData: [],
+            popularRoutesChartData: [],
+            popularRoutesThisMonthChartData: [],
             lang: lang,
         }
     },
     mounted() {
         // mockup
-        this.chartData = [
+        this.popularCitiesChartData = [
             { label: 'Darfur', value: 74 },
             { label: 'Congo', value: 37 },
             { label: 'Slavekysten', value: 15 },
             { label: 'Luanda', value: 46 },
             { label: 'Other', value: 27 },
         ]
+        this.popularRoutesChartData = [
+            { name: 'Sahara - Darfur', thisMonth: 45, total: 127 },
+            { name: 'Congo - Luanda', thisMonth: 66, total: 100 },
+            { name: 'Wadai - Darfur', thisMonth: 15, total: 87 },
+            { name: 'Mocambique - Zanzibar', thisMonth: 2, total: 53 },
+        ]
+        this.popularRoutesThisMonthChartData = this.popularRoutesChartData.map(
+            (entry) => ({ thisMonth: entry.thisMonth })
+        )
     },
 }
 </script>
